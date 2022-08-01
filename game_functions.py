87da07c5b5c53d68e812dadc0a20d8189fb68492
Olga Alekhina <alekhina.olga@gmail.com>
2022-08-01 17:22:10 +0300

    add functions

---
+++ game_functions.py
@@ -0,0 +1,56 @@
+def get_board(b):
+    print('    0     1     2')
+    for j in range(3):
+        print(j, end=' ')
+        for i in range(3):
+            print(' ', b[3 * j + i], ' ', end=' ')
+        print('\n')
+
+
+def make_move(player, b):
+    print(f'Ваша фигура - "{player}".')
+    while True:
+        move = input("Введите через пробел номер столбца и номер строки, после чего нажмите ENTER: ").split()
+        if move[0] not in ('0', '1', '2') or move[1] not in ('0', '1', '2'):
+            print(f'Ваш ход должен лежать в диапазоне от 0 до 2')
+        elif b[3 * int(move[1]) + int(move[0])] != '-':
+            print('Эта клетка уже занята!')
+        else:
+            b[3 * int(move[1]) + int(move[0])] = player
+            break
+    return b
+
+
+def check_win(b):
+    win = [[b[0], b[1], b[2]], [b[3], b[4], b[5]], [b[6], b[7], b[8]], [b[0], b[3], b[6]],
+           [b[1], b[4], b[7]], [b[2], b[5], b[8]], [b[0], b[4], b[8]], [b[2], b[4], b[6]]]
+    for elem in win:
+        if elem[0] == elem[1] == elem[2] != '-':
+            return True
+
+
+def game():
+    b = ['-', '-', '-', '-', '-', '-', '-', '-', '-']
+    players = ['x', '0']
+    winner = None
+
+    player = input(f'Начинаем игру "крестики-нолики". Выберите фигуру - "x" или "0": ')
+    while player not in ('x', '0'):
+        player = input("Ошибка! Вы должны выбрать 'x' или '0. Попробуйте еще раз: ")
+
+    while winner is None:
+        if b.count('-') != 0:
+            get_board(b)
+            make_move(player, b)
+            player = players[0] if player == players[1] else players[1]
+            winner = check_win(b)
+            if winner:
+                print("Игра закончена! Вы победили!")
+
+        else:
+            print('Вы сыграли вничью!')
+            break
+
+
+game()
+
